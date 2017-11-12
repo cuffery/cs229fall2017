b8c4c5f4a64d878010b953ba28a4efacfcfc7a6d
Yi Zhong <yizhong@Yis-MacBook-Pro.local>
2017-11-12 13:32:30 -0800

    update processing script

--- yi_processing/yizhon_process_scratch.py
+++ yi_processing/yizhon_process_scratch.py
@@ -5,13 +5,39 @@ import scipy
 import pandas as pd
 
 #match_data = pd.read_csv('charting-m-matches.csv')
-match_data = pd.read_csv('charting-m-matches.csv', delimiter=",",quoting=3, error_bad_lines=False, encoding = "ISO-8859-1")
-point_data = pd.read_csv('charting-m-points.csv', delimiter=",",quoting=3, error_bad_lines=False, encoding = "ISO-8859-1")
+match_data = pd.read_csv('/Users/yizhong/Documents/cs229fall2017/tennis_MatchChartingProject/charting-m-matches.csv', delimiter=",",quoting=3, error_bad_lines=False, encoding = "ISO-8859-1")
+point_data = pd.read_csv('/Users/yizhong/Documents/cs229fall2017/tennis_MatchChartingProject/charting-m-points.csv', delimiter=",",quoting=3, error_bad_lines=False, encoding = "ISO-8859-1")
 
-#train = np.genfromtxt('charting-m-matches.csv', dtype = mystr , skip_header=True, delimiter=',')
+match_data.loc[match_data['Pl 1 hand']=='R','player_1_right']=1
+match_data.loc[match_data['Pl 2 hand']=='R','player_2_right']=1
 
+match_data.loc[match_data['Surface']=='Hard','surface_hard']=1
+match_data.loc[match_data['Surface']=='Grass','surface_grass']=1
+match_data.loc[match_data['Surface']=='Clay','surface_clay']=1
+
+
+
+
+print(match_data.iloc[0])
 print(match_data.iloc[1])
-print(point_data.iloc[1])
+
+df = pd.DataFrame()
+df['match_id'] = match_data['match_id']
+df['player_1'] = match_data['Player 1']
+df['player_2'] = match_data['Player 2']
+
+df['player_1_right'] = match_data['player_1_right']
+df['player_2_right'] = match_data['player_2_right']
+df['tournament'] = match_data['Tournament']
+df['round'] = match_data['round']
+
+
+
+
+print(df)
+
+
+
 
 #print(train)
 
