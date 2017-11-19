aa7528d618a0696d36a246d387c0be614f703aa6
Yi Zhong <yizhon@microsoft.com>
2017-11-18 20:29:57 -0800

    yiyiy

--- yi_processing/combine_match_players.py
+++ yi_processing/combine_match_players.py
@@ -36,40 +36,34 @@ def getPlayerIDTable(player_ranking,match_details):
     df = pd.merge(player_from_match,player_from_ranking, on = ['player_lname','player_fname','player_finit'],how = 'left',validate = 'one_to_many')
     df = df[pd.notnull(df['player_id'])]
     df['player_concat'] = df['player_fname'] + df['player_lname'] + df['player_finit']
-    return df[['player_concat','player_id']]    
+    return df[['player_concat','player_id']] 
 
-def mergeMatchRanking(player_ranking,match_details):
-    player_id_table = getPlayerIDTable(player_ranking,match_details)
-    print(player_id_table)
-
+def getPlayerID(player_id_table,match_details):   
     match_details['player1_concat'] = match_details['player_1_fname'] + match_details['player_1_lname'] + match_details['player_1_finit']
     match_details['player2_concat'] = match_details['player_2_fname'] + match_details['player_2_lname'] + match_details['player_2_finit']
 
     match_details = match_details.merge(player_id_table,left_on='player1_concat',right_on = 'player_concat',how = 'left')
-    match_details=match_details.rename(columns = {'player_id':'player_1_id'})
+    match_details = match_details.rename(columns = {'player_id':'player_1_id'})
     
     match_details = match_details.merge(player_id_table,left_on='player2_concat',right_on = 'player_concat',how = 'left')
-    match_details=match_details.rename(columns = {'player_id':'player_2_id'})
+    match_details = match_details.rename(columns = {'player_id':'player_2_id'})
 
     match_details =  match_details.drop(['player_concat_x','player_concat_y','player1_concat','player2_concat'],axis = 1)
-    print(match_details.iloc[0])
-    #getPlayer(player_ranking[player_info],match_details[match_player1_info].iloc[0])
+    return match_details
+
+def mergeMatchRanking(player_ranking,match_details):
+    player_id_table = getPlayerIDTable(player_ranking,match_details)
+
+    match_details = getPlayerID(player_id_table,match_details)
 
 
-    #1. find match with first init and last name
-
-    #2. if more than 1 distinct, then match first name and last name
-
-    #3. find closest date
-    #matched_ranking_date = player_ranking[player_ranking['player_lname']=='Federer']['ranking_date']
-
-    #print(getClosestDate(match_details[match_details['player_2_lname']=='Federer']['Date'].iloc[0],matched_ranking_date))
-
-    return
+    return match_details
 
 def main():
     player_ranking, match_details = importProcessed()
-    mergeMatchRanking(player_ranking,match_details)
+    match_details = mergeMatchRanking(player_ranking,match_details)
+    match_details.to_csv('joined_player_match.csv', sep=",",quoting=3, encoding = "ISO-8859-1")
+
     return
 
 if __name__ == '__main__':
