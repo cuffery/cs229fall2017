815fea1394c987ca1a605cece3d1660b0e55160f
Yi Zhong <yizhon@microsoft.com>
2017-11-19 18:27:49 -0800

    gosh i need to change computer again

--- master_join.py
+++ master_join.py
@@ -1,10 +1,5 @@
 import numpy as np
 import pandas as pd
-import CommonOpponentStats
-import GenerateATPlabel
-
-CommonOpponentStats.main()
-GenerateATPlabel.main()
 
 joined_hist_match = pd.read_csv('../ec_processing/joined_hist_match.csv',
                     index_col=None,
@@ -14,5 +9,10 @@ atp_label = pd.read_csv('../ec_processing/match_label_ATP.csv',
                         index_col=None,
                         header=0)
 
+curr_match_data = pd.read_csv('../yubo_processing/current_match_data.csv',
+                        index_col=None,
+                        header=0)
+
 print(list(joined_hist_match))
 print(list(atp_label))
+print(list(curr_match_data))
