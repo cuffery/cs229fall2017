2cf0be5208075295740ab2fd42ccda130dbee48d
Yubo Tian <yubo.d.tian@gmail.com>
2017-11-19 17:05:24 -0800

    fixed bug in label_match

--- yubo_processing/label_match.py
+++ yubo_processing/label_match.py
@@ -9,17 +9,37 @@ import time
 
 def processData(data_):
     # rm matches before 2000 and keep only relevant cols
-    data = data_[(data_['match_id'].map(lambda x: x.startswith('20')))][['match_id','Set1','Set2']].apply(pd.to_numeric, errors='ignore')
+    data = data_[(data_['match_id'].map(lambda x: x.startswith('20')))][['match_id','Set1','Set2','Gm1','Gm2','Pts','Svr']].apply(pd.to_numeric, errors='ignore')
 
     grouped = data.groupby('match_id')
 
     r = pd.DataFrame()
 
     for match, group in grouped:
+        winner = 0
+        if group.Set1.max() > group.Set2.max():
+            winner = 1
+        elif group.Set1.max() < group.Set2.max():
+            winner = 2
+        else:
+            if (group.Gm1.iloc[-1] > group.Gm2.iloc[-1]):
+                 winner = 1
+            elif (group.Gm1.iloc[-1] < group.Gm2.iloc[-1]):
+                 winner = 2
+            else:
+                server_pts = int(group.Pts.iloc[-1].split('-')[0])
+                receiver_pts = int(group.Pts.iloc[-1].split('-')[-1])
+                if (server_pts > receiver_pts):
+                    winner = group.Svr.iloc[-1]
+                elif (server_pts < receiver_pts):
+                    winner = group.Svr.iloc[-1] * -1 + 3
+                else:
+                    print ('unexpected match result')
+
         p = pd.DataFrame({
             'match_id' : grouped.get_group(match).match_id.unique(),
-            'match_winner' : 1 if group.Set1.max() > group.Set2.max() else 2
-         })
+            'match_winner' : winner
+        })
 
         r = r.append(p)
     return r
