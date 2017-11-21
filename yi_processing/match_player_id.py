81e8bda25af8eb7a2648fd4c9a017db50bff77c9
Yubo Tian <yubo.d.tian@gmail.com>
2017-11-20 22:37:50 -0800

    finalized join strategy...

--- yi_processing/match_player_id.py
+++ yi_processing/match_player_id.py
@@ -66,12 +66,28 @@ def findPlayerIdForMatches(match_data):
     # dup = match_data[match_data.duplicated(['player_1_lname','player_2_lname','Date'],keep = False)]
     # dup.to_csv('dup.csv')
 
-    print('match data',match_data.shape[0])
+    # print('match data',match_data.shape[0])
     # print('player_1 = winner',join_winner_1_loser_2.shape[0])
     # print('player_2 = winner',join_winner_2_loser_1.shape[0])
     # join_winner_1_loser_2.to_csv('debug_join.csv')
 
-    print(match_data)
+    # get player name - id dictionary from file
+    raw_player_data = pd.read_csv('../tennis_atp/atp_players.csv', delimiter=",",quoting=3, encoding = "ISO-8859-1")
+
+    print(raw_player_data.shape)
+    print(list(raw_player_data))
+    
+    raw_player_data.columns = ['player_id', 'first_name', 'last_name', 'hand', 'dob', 'country']
+
+    print(raw_player_data.shape)
+    print(list(raw_player_data))
+    
+    v = raw_player_data.groupby(['first_name','last_name']).size()
+
+    c = raw_player_data[['first_name', 'last_name','hand']].drop_duplicates().shape
+
+    print(c)
+
 
     return
 
