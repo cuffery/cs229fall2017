327d0910895e1da1f852adcd7d092a1576e0baa9
yzhong94 <yizhon@microsoft.com>
2017-12-10 12:23:25 -0800

    modify model evaluation

--- model_evaluation.py
+++ model_evaluation.py
@@ -6,11 +6,14 @@ import numpy as np
 import scipy
 from sklearn.model_selection import learning_curve
 from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 import logistic
 import svm
 import joined_data_pred
+import model_selection
 
 
 def plot_learning_curve():
@@ -31,18 +34,32 @@ def main():
 
     y = data['label']
 
-    drop_col = ['label','surface_hard', 'surface_grass', 'surface_clay']
+    #drop_col = ['label','surface_hard', 'surface_grass', 'surface_clay']
+    drop_col = ['label']
+
     X = data.drop(drop_col,axis = 1).dropna(axis=0, how='any')
 
     # [TODO: remove extra col-dropping] this is trying to speed things up a bit temporarily
-    temp_drop = ['ace_diff', 'first_serve_perc_diff', 'height_diff', 'match_num', 'opponent_ID', 'pts_1st_srv_pct', 'pts_2nd_srv_pct', 'rcv_pts_pct', 'ttl_pts_won']
-    X = data.drop(temp_drop,axis = 1).dropna(axis=0, how='any')
+    #temp_drop = ['ace_diff', 'first_serve_perc_diff', 'height_diff', 'match_num', 'opponent_ID', 'pts_1st_srv_pct', 'pts_2nd_srv_pct', 'rcv_pts_pct', 'ttl_pts_won']
+    #X = data.drop(temp_drop,axis = 1).dropna(axis=0, how='any')
 
-    train_sizes_svm, train_scores_svm, valid_scores_svm = learning_curve(SVC(kernel='linear'), X, y, train_sizes=[1000, 2000, 4000], cv=5)
+    rfe = model_selection.getRFE(LinearSVC(),20)
+    rfe = rfe.fit(X,y)
 
-    print("train_sizes_svm\n", train_sizes_svm)
-    print("train_scores_svm\n", train_scores_svm)
-    print("valid_scores_svm\n", valid_scores_svm)
+    train_sizes_lin_svc, train_scores_lin_svc, valid_scores_lin_svc = learning_curve(LinearSVC(), rfe.transform(X), y, train_sizes=[1000, 2000, 4000], cv=5)
+
+    print("train_sizes_lin_svc\n", train_sizes_lin_svc)
+    print("train_scores_lin_svc\n", train_scores_lin_svc)
+    print("valid_scores_lin_svc\n", valid_scores_lin_svc)
+
+    rfe = model_selection.getRFE(LinearSVC(),20)
+    rfe = rfe.fit(X,y)
+
+    train_sizes_svm, train_scores_svm, valid_scores_svm = learning_curve(SVC(), rfe.transform(X), y, train_sizes=[1000, 2000, 4000], cv=5)
+
+    print("train_sizes_svc\n", train_sizes_svm)
+    print("train_scores_svc\n", train_scores_svm)
+    print("valid_scores_svc\n", valid_scores_svm)
 
     #plot_learning_curve()
     print(list(X))
