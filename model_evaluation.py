650e4d4f5d17d31c30b77ffc0e564b3282259157
Yi Zhong <yizhon@microsoft.com>
2017-12-15 10:06:45 -0800

    modify model_evaluation

--- model_evaluation.py
+++ model_evaluation.py
@@ -26,7 +26,6 @@ import svm
 import joined_data_pred
 import model_selection
 
-
 def plot_learning_curve(train_sizes, train_scores, test_scores, filename, debug):
     train_scores_mean = np.mean(train_scores, axis=1)
     train_scores_std = np.std(train_scores, axis=1)
@@ -95,73 +94,6 @@ def get_learning_curve_plots(data_split,num_features):
     plot_learning_curve(train_sizes_lr, train_scores_lr, valid_scores_lr, 'LR_learning_curve.png', 3)
     return
 
-def getConfusionMatrix(data_split,num_features, model):
-    after_set_1_train= data_split['after_set_1_train']
-    after_set_2_train = data_split['after_set_2_train']
-    after_set_1_all_train = data_split['after_set_1_all_train']
-    after_set_2_all_train = data_split['after_set_2_all_train']
-    after_set_1_dev = data_split['after_set_1_dev']
-    after_set_2_dev = data_split['after_set_2_dev']
-
-    all_train = data_split['all_train']
-    test = data_split['test']
- 
-    train = data_split['train']
-    dev = data_split['dev']
-    drop_col = ['label']
-
-    hist = ['1st_serve_pts_won_diff', '2nd_serve_pts_won_diff', 'SvGms_diff', 'ace_diff', 'bp_faced_diff', 'bp_saved_diff', 'bp_saving_perc_diff', 'df_diff', 'duration_minutes', 'first_serve_perc_diff', 'height_diff', 'match_num', 'opponent_ID', 'rank_diff', 'rank_pts_diff', 'same_handedness', 'svpt_diff']
-    curr = ['surface_hard', 'surface_grass', 'surface_clay','aces', 'dfs', 'unforced', '1st_srv_pct', 'bk_pts', 'winners', 'pts_1st_srv_pct', 'pts_2nd_srv_pct', 'rcv_pts_pct','ttl_pts_won']
-
-    #historical confusion matrix
-    svc = model
-    rfe = model_selection.getRFE(model,num_features)
-    rfe = rfe.fit(train[hist],train['label'])
-    svc.fit(rfe.transform(train[hist]),train['label'])
-    pred = svc.predict(rfe.transform(dev[hist]))
-    
-    tn, fp, fn, tp = confusion_matrix(dev['label'],pred).ravel()
-    print('Historical')
-    print('tn, fp, fn, tp',(tn, fp, fn, tp))
-
-    #current only after set 1 confusion matrix
-    svc = model
-    rfe = model_selection.getRFE(model,num_features)
-    rfe = rfe.fit(after_set_1_train[curr],after_set_1_train['label'])
-    svc.fit(rfe.transform(after_set_1_dev[curr]),after_set_1_dev['label'])
-    pred = svc.predict(rfe.transform(after_set_1_dev[curr]))
-    
-    tn, fp, fn, tp = confusion_matrix(after_set_1_dev['label'],pred).ravel()
-    print('Current only after set 1')
-    print('tn, fp, fn, tp',(tn, fp, fn, tp))
-
-    #current + historical only after set 1 confusion matrix
-    svc = model
-    X = after_set_1_train.drop(drop_col,axis = 1).dropna(axis=0, how='any')
-    y = after_set_1_train['label']
-    rfe = model_selection.getRFE(model,num_features)
-    rfe = rfe.fit(X,y)
-    svc.fit(rfe.transform(X),y)
-    pred = svc.predict(rfe.transform(after_set_1_dev.drop(drop_col,axis = 1).dropna(axis=0, how='any')))
-    
-    tn, fp, fn, tp = confusion_matrix(after_set_1_dev['label'],pred).ravel()
-    print('Current + historical after set 1')
-    print('tn, fp, fn, tp',(tn, fp, fn, tp))
-
-    #current + historical only after set 2 confusion matrix
-    svc = model
-    X = after_set_2_train.drop(drop_col,axis = 1).dropna(axis=0, how='any')
-    y = after_set_2_train['label']
-    rfe = model_selection.getRFE(model,15)
-    rfe = rfe.fit(X,y)
-    svc.fit(rfe.transform(X),y)
-    pred = svc.predict(rfe.transform(after_set_2_dev.drop(drop_col,axis = 1).dropna(axis=0, how='any')))
-    
-    tn, fp, fn, tp = confusion_matrix(after_set_2_dev['label'],pred).ravel()
-    print('Current + historical after set 2')
-    print('tn, fp, fn, tp',(tn, fp, fn, tp))
-    return
-
 def outputRanking(model,feature, label,filename):
     rfe = model_selection.getRFE(model,1)
     ranking = model_selection.getRFERanking(rfe,feature, label)
@@ -177,7 +109,11 @@ def getData(diff):
     drop_col = ['after_set']
 
     data = joined_data_pred.cleanDataForSklearn(data_)
-
+    '''
+    #remove newly added features
+    drop_new_col = ['avg_minutes_lost','avg_minutes_won','avg_age_diff_lost_to','avg_age_diff_won_against','best_of','match_day_age','opponent_match_day_age','rally_sum']
+    data = data.drop(drop_new_col,axis = 1).dropna(axis=0, how='any')
+    '''
     all_train, test = train_test_split(data, test_size=0.20, random_state=520)
     train, dev = train_test_split(all_train, test_size=0.25, random_state=520)
 
@@ -327,106 +263,158 @@ def main():
     
     data_split = getData(diff = False)
     print(list(data_split['train']))
+    i = 0
+
+    result_model_hist = []
+    result_model_curr = []
+    result_model_hist_curr1 = []
+    result_model_hist_curr2 = [] 
+    max_iter = 25
+
+    conf_model_hist = np.array((max_iter,4))
+    conf_model_curr = np.array((max_iter,4))
+    conf_model_hist_curr1 = np.array((max_iter,4))
+    conf_model_hist_curr2 = np.array((max_iter,4))
+    '''
+    hist = ['1st_serve_pts_won_diff', '2nd_serve_pts_won_diff', 'SvGms_diff', 'ace_diff', 'bp_faced_diff', 'bp_saved_diff', 'bp_saving_perc_diff', 'df_diff', 'duration_minutes', 'first_serve_perc_diff', 'height_diff', 'match_num', 'opponent_ID', 'rank_diff', 'rank_pts_diff', 'same_handedness', 'svpt_diff']
+    curr = ['surface_hard', 'surface_grass', 'surface_clay','aces', 'dfs', 'unforced', '1st_srv_pct', 'bk_pts', 'winners', 'pts_1st_srv_pct', 'pts_2nd_srv_pct', 'rcv_pts_pct']
+    '''
     '''
     #----FEATURE ANALYSIS AND LEARNING CURVE----#
     #Get graphs with feature analysis and learning curve
     #This script outputs graphs used in report and poster
     graph.main()
     '''
-    #----EXPLORE DIFFERENT MODELS FOR FOUR DATA MODEL----#
-    #Using hist + after 1st set
-    print('-----Data model: using current + after 1st set-----')
+    while i < max_iter:
+        #----EXPLORE DIFFERENT MODELS FOR FOUR DATA MODEL----#
+        #Using hist + after 1st set
+        print('-----Data model: using current + after 1st set-----')
 
-    feature = data_split['after_set_1_all_train'].drop(['label'],axis = 1)
-    label = data_split['after_set_1_all_train']['label']
-    
-    getAccAlg(feature,label)
+        feature = data_split['after_set_1_all_train'].drop(['label'],axis = 1)
+        label = data_split['after_set_1_all_train']['label']
+        
+        getAccAlg(feature,label)
 
-    #best model is LinearSVC
-    model = LinearSVC()
-    rfe = model_selection.getRFE(LinearSVC(),15)
-    rfe.fit(feature,label)
-    feature_t = rfe.transform(feature)
+        #best model is LinearSVC
+        model = LinearSVC()
+        rfe = model_selection.getRFE(LinearSVC(),15)
+        rfe.fit(feature,label)
+        feature_t = rfe.transform(feature)
 
-    model.fit(feature_t,label)
+        model.fit(feature_t,label)
 
-    test = data_split['test']
-    feature_test = test[test['after_set']==1].drop(['label','after_set'],axis = 1)
-    label_test = test[test['after_set']==1]['label']
+        test = data_split['test']
+        feature_test = test[test['after_set']==1].drop(['label','after_set'],axis = 1)
+        label_test = test[test['after_set']==1]['label']
 
-    print('Test score = ',model.score(rfe.transform(feature_test),label_test))
+        print('Test score = ',model.score(rfe.transform(feature_test),label_test))
+        result_model_hist_curr1.append(model.score(rfe.transform(feature_test),label_test))
+        print(result_model_hist_curr1)
 
-    #Using hist + after 2nd set
-    print('-----Data model: using current + after 2nd set-----')
+        #print confusion matrix for best model
+        tn, fp, fn, tp = confusion_matrix(model.predict(rfe.transform(feature_test)),label_test).ravel()
+        print('Hist + Current after set 1')
+        print('tp, fp, tn, fp',(tp, fp, tn, fp))
+        conf_model_hist_curr1[i,0] = tp/sum((tp, fp, tn, fp))
+        print(conf_model_hist_curr1)
+        #Using hist + after 2nd set
+        print('-----Data model: using current + after 2nd set-----')
 
-    feature = data_split['after_set_2_all_train'].drop(['label'],axis = 1)
-    label = data_split['after_set_2_all_train']['label']
-    
-    getAccAlg(feature,label)
+        feature = data_split['after_set_2_all_train'].drop(['label'],axis = 1)
+        label = data_split['after_set_2_all_train']['label']
+        
+        getAccAlg(feature,label)
 
-    #best model is linear SVC
-    model = LinearSVC()
-    rfe = model_selection.getRFE(LinearSVC(),15)
-    rfe.fit(feature,label)
-    feature_t = rfe.transform(feature)
+        #best model is linear SVC
+        model = LinearSVC()
+        rfe = model_selection.getRFE(LinearSVC(),15)
+        rfe.fit(feature,label)
+        feature_t = rfe.transform(feature)
 
-    model.fit(feature_t,label)
+        model.fit(feature_t,label)
 
-    test = data_split['test']
-    feature_test = test[test['after_set']==2].drop(['label','after_set'],axis = 1)
-    label_test = test[test['after_set']==2]['label']
+        test = data_split['test']
+        feature_test = test[test['after_set']==2].drop(['label','after_set'],axis = 1)
+        label_test = test[test['after_set']==2]['label']
 
-    print('Test score = ',model.score(rfe.transform(feature_test),label_test))
+        print('Test score = ',model.score(rfe.transform(feature_test),label_test))
+        result_model_hist_curr2.append(model.score(rfe.transform(feature_test),label_test))
+        #print confusion matrix for best model
+        tn, fp, fn, tp = confusion_matrix(model.predict(rfe.transform(feature_test)),label_test).ravel()
+        print('Hist + Current after set 2')
+        print('tp, fp, tn, fp',(tp, fp, tn, fp))
 
-    #Using historical only
-    print('-----Data model: historical data only-----')
+        #Using historical only
+        print('-----Data model: historical data only-----')
 
-    feature = data_split['all_train'][hist]
-    label = data_split['all_train']['label']
-    
-    getAccAlg(feature,label)
+        feature = data_split['all_train'][hist]
+        label = data_split['all_train']['label']
+        
+        getAccAlg(feature,label)
 
-    #best model is SVC + rbf kernel
-    model = SVC(kernel = 'rbf')
+        #best model is SVC + rbf kernel
+        model = SVC(kernel = 'rbf')
 
-    rfe = model_selection.getRFE(LinearSVC(),15)
-    rfe.fit(feature,label)
-    feature_t = rfe.transform(feature)
+        rfe = model_selection.getRFE(LinearSVC(),15)
+        rfe.fit(feature,label)
+        feature_t = rfe.transform(feature)
 
-    model.fit(feature_t,label)
+        model.fit(feature_t,label)
 
-    test = data_split['test']
-    feature_test = test[hist]
-    label_test = test['label']
+        test = data_split['test']
+        feature_test = test[hist]
+        label_test = test['label']
 
-    print('Test score = ',model.score(rfe.transform(feature_test),label_test))
-    #Using current after set 1 only
-    print('-----Data model: current data only-----')
+        print('Test score = ',model.score(rfe.transform(feature_test),label_test))
+        result_model_hist.append(model.score(rfe.transform(feature_test),label_test))
 
-    feature = data_split['all_train'][curr]
-    label = data_split['all_train']['label']
-    
-    getAccAlg(feature,label)
+        #print confusion matrix for best model
+        tn, fp, fn, tp = confusion_matrix(model.predict(rfe.transform(feature_test)),label_test).ravel()
+        print('Historical only')
+        print('tp, fp, tn, fp',(tp, fp, tn, fp))
 
-    #best model is linear discriminant analysis with feature size = 15
 
-    model = LogisticRegression()
+        #Using current after set 1 only
+        print('-----Data model: current data only-----')
 
-    rfe = model_selection.getRFE(LogisticRegression(),15)
-    rfe.fit(feature,label)
-    feature_t = rfe.transform(feature)
+        feature = data_split['after_set_1_all_train'][curr]
+        label = data_split['after_set_1_all_train']['label']    
+        getAccAlg(feature,label)
 
-    model.fit(feature_t,label)
+        #best model is linear discriminant analysis with feature size = 15
 
-    test = data_split['test']
-    feature_test = test[curr]
-    label_test = test['label']
+        model = LogisticRegression()
 
-    print('Test score = ',model.score(rfe.transform(feature_test),label_test))
-    '''
+        rfe = model_selection.getRFE(LogisticRegression(),15)
+        rfe.fit(feature,label)
+        feature_t = rfe.transform(feature)
+
+        model.fit(feature_t,label)
+
+        test = data_split['test']
+        feature_test = test[test['after_set']==1][curr]
+        label_test = test[test['after_set']==1]['label']
+        print('Test score = ',model.score(rfe.transform(feature_test),label_test))
+        result_model_curr.append(model.score(rfe.transform(feature_test),label_test))
+
+        #print confusion matrix for best model
+        tn, fp, fn, tp = confusion_matrix(model.predict(rfe.transform(feature_test)),label_test).ravel()
+        print('Current after set 1')
+        print('tp, fp, tn, fp',(tp, fp, tn, fp))
+
+        i += 1
+    print("test accuracy for historical only")
+    print(result_model_hist)
+    print("test accuracy for current only")
+    print(result_model_curr)
+    print("test accuracy for current set 1 + historical")
+    print(result_model_hist_curr1)
+    print("test accuracy for current set 2 + historical")
+    print(result_model_hist_curr2)
     #----GET CONFUSION MATRIX FOR BEST MODEL----#
     #getConfusionMatrix(data_split,num_features = 15,model = LinearSVC())
     
+    '''
     #----GET FEATURE RANKING FOR DIFFERENT DATA MODEL----#
     #historical + current 
     #after set 1 with logistic regression
